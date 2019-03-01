69e71e328b0076feb19698f0a729f298067a2635
alevada <vadasanmaria@gmail.com>
2019-03-01 15:37:32 +0200

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  #validates() method is the standard Rails validator. It checks one or more model fields against one or more conditions
+  validates :title, :description, :image_url,
+            # presence: true >>tells the validator to check that each of the named fields is present and that its contents aren’t empty.
+            presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with:    %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
 end
